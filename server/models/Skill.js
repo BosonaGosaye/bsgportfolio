d8f65935bfc65ce95d12cda09f228b4c6beb33c7
Bosona Gosaye <161635012+BosonaGosaye@users.noreply.github.com>
2026-01-31 07:38:50 +0300

    chnge

--- server/models/Skill.js
+++ server/models/Skill.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const skillSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, required: true, enum: ['Frontend', 'Backend', 'Tools', 'Design', 'Other'] },
+  category: { type: String, required: true, enum: ['Frontend', 'Backend', 'Mobile Application', 'Tools & Technologies', 'Database & Cloud', 'DevOps & Deployment', 'Testing & Debugging', 'Design', 'Soft Skills', 'Other'] },
   percentage: { type: Number, min: 0, max: 100 },
   icon: { type: String }
 }, { timestamps: true });
